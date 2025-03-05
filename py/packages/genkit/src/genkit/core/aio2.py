aef7ae409e50193d1c1de31ded5b42c01c8d8f02
Yesudeep Mangalapilly <yesudeep@google.com>
2025-03-05 01:30:12 -0800

    feat: aio

--- py/packages/genkit/src/genkit/core/aio2.py
+++ py/packages/genkit/src/genkit/core/aio2.py
@@ -4,42 +4,43 @@
 """Asyncio helpers."""
 
 import asyncio
-from asyncio import Future, ensure_future, wait
-from typing import Any, AsyncIterator, TypeVar
+from asyncio import FIRST_COMPLETED, Future, Queue, ensure_future, wait
+from typing import Any, AsyncIterator
 
 
-T = TypeVar('T')
+class Channel(AsyncIterator[Any]):
+    """
+    An asynchronous channel for sending and receiving values.
 
-
-class Channel(AsyncIterator[T]):
-    """An asynchronous channel for sending and receiving values.
-
-    This class provides an asynchronous queue-like interface, allowing values to
-    be sent and received between different parts of an asynchronous program. It
-    also supports closing the channel, which will signal to any receivers that
-    no more values will be sent.
+    This class provides an asynchronous queue-like interface, allowing
+    values to be sent and received between different parts of an
+    asynchronous program. It also supports closing the channel,
+    which will signal to any receivers that no more values will be sent.
     """
 
     def __init__(self, maxsize: int = 0):
-        """Initializes a new Channel.
+        """
+        Initializes a new Channel.
 
-        The channel is initialized with an internal queue to store values, a
-        future to signal when the channel is closed, and an optional close
-        future.
+        The channel is initialized with an internal queue to store values,
+        a future to signal when the channel is closed, and an optional
+        close future.
 
         Args:
-            maxsize: The maximum size of the queue. Defaults to 0 (unlimited).
+            maxsize (int, optional): The maximum size of the queue. Defaults to
+                0 (unlimited).
         """
-        self.queue: asyncio.Queue[T] = asyncio.Queue(maxsize=maxsize)
-        self.closed: asyncio.Future[Any] = asyncio.Future()
-        self.__close_future: asyncio.Future[Any] | None = None
+        self.queue = Queue(maxsize=maxsize)
+        self.closed = Future()
+        self.__close_future = None
 
     def __aiter__(self):
         """Returns self as an async iterator."""
         return self
 
-    async def __anext__(self) -> T:
-        """Retrieves the next value from the channel.
+    async def __anext__(self) -> Any:
+        """
+        Retrieves the next value from the channel.
 
         If the queue is not empty, the value is returned immediately.
         Otherwise, it waits until a value is available or the channel is closed.
@@ -49,7 +50,7 @@ class Channel(AsyncIterator[T]):
                                 are available.
 
         Returns:
-            The next value from the channel.
+            Any: The next value from the channel.
         """
         if not self.queue.empty():
             value = self.queue.get_nowait()
@@ -68,7 +69,7 @@ class Channel(AsyncIterator[T]):
 
         try:
             finished, _ = await wait(
-                [pop, self.__close_future], return_when=asyncio.FIRST_COMPLETED
+                [pop, self.__close_future], return_when=FIRST_COMPLETED
             )
             if pop in finished:
                 return pop.result()
@@ -80,8 +81,9 @@ class Channel(AsyncIterator[T]):
             pop.cancel()
             raise StopAsyncIteration
 
-    def send(self, value: T):
-        """Sends a value into the channel.
+    def send(self, value: Any):
+        """
+        Sends a value into the channel.
 
         The value is added to the internal queue.
 
@@ -90,11 +92,12 @@ class Channel(AsyncIterator[T]):
         """
         return self.queue.put_nowait(value)
 
-    def set_close_future(self, future: asyncio.Future[Any]):
-        """Sets a future that, when completed, will close the channel.
+    def set_close_future(self, future: Future):
+        """
+        Sets a future that, when completed, will close the channel.
 
         Args:
-            future: The future to set.
+            future (Future): The future to set.
         """
         self.__close_future = ensure_future(future)
 
@@ -114,7 +117,8 @@ class Channel(AsyncIterator[T]):
         self.__close_future.add_done_callback(cleanup)
 
     async def __pop(self) -> Any:
-        """Asynchronously retrieves a value from the queue.
+        """
+        Asynchronously retrieves a value from the queue.
 
         This method waits until a value is available in the queue.
 
